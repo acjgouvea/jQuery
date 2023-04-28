f559566a078a751df80c37fae8cb9762d68974e9
carlos gouvea <108358794+acjgouvea@users.noreply.github.com>
2023-04-27 21:44:40 -0300

    fim do exercicio

--- main.js
+++ main.js
@@ -8,6 +8,18 @@ $('#botao-cancelar').click(function(){
 
 $('form').on('submit', function(e){
     e.preventDefault();
+    const novaImagem = $('#nova-imagem').val();
+    const novoItem =  $('<li style="display-none"></li>');
+    $(`<img src="${novaImagem}"/>`).appendTo(novoItem);
+    $(`
+    <div class="overlay-imagem-link"> 
+    <a href="${novaImagem}" target="_blank" title="Ver imagem em tamanho real">
+        Ver imagem em tamanho real </a>
+    </div>    
+    `).appendTo(novoItem);
+    $(novoItem).appendTo('ul');
+    $(novoItem).fadeIn(1000);
+    $('#nova-imagem').val('')
 })
 
 })
